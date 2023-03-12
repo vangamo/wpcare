fe0932158c83feb82c880fdb0386518ee0f9be7e
VanGamo <vangamo.beta@gmail.com>
2023-03-12 18:04:58 +0100

    Refactor functions into components.

--- frontend_admin/src/components/table/table.jsx
+++ frontend_admin/src/components/table/table.jsx
@@ -2,24 +2,34 @@ import { useState } from 'preact/hooks';
 import PropTypes from 'prop-types';
 import './table.scss';
 
-function TableColumns(columns) {
+const COLUMN_TYPE = PropTypes.exact({
+  heading: PropTypes.string,
+  type: PropTypes.string,
+  col: PropTypes.string,
+  colLink: PropTypes.string,
+});
+
+const DATA_TYPE = PropTypes.object;
+
+const DETAILS_SECTION_TYPE = PropTypes.oneOfType([PropTypes.element, PropTypes.string, PropTypes.node, PropTypes.func]);
+
+function TableColumns({ columns }) {
   return columns.map((col, idx) =>
-    col.type === 'checkbox' ? (
-      <th key={'col-' + idx}></th>
-    ) : (
-      <th key={'col-' + idx}>{col.heading}</th>
-    )
+    col.type === 'checkbox' ? <th key={'col-' + idx}></th> : <th key={'col-' + idx}>{col.heading}</th>
   );
 }
+TableColumns.propTypes = {
+  columns: PropTypes.arrayOf(
+    COLUMN_TYPE
+  ).isRequired,
+};
 
 const ROW_VALUE_RENDERER = {
   checkbox: (rowValues, columnDef) => {
-    return <input type='checkbox' />;
+    return <input type="checkbox" />;
   },
   link: (rowValues, columnDef) => {
-    return (
-      <a href={rowValues[columnDef.colLink]}>{rowValues[columnDef.col]}</a>
-    );
+    return <a href={rowValues[columnDef.colLink]}>{rowValues[columnDef.col]}</a>;
   },
   text: (rowValues, columnDef) => {
     return rowValues[columnDef.col];
@@ -29,7 +39,7 @@ const ROW_VALUE_RENDERER = {
   },
 };
 
-function TableRow(row, columns, rowIdx) {
+function TableRow({ row, columns, rowIdx }) {
   return columns.map((columnDef, colIdx) => (
     <td key={'row-' + rowIdx + colIdx}>
       {ROW_VALUE_RENDERER[columnDef.type]
@@ -39,13 +49,21 @@ function TableRow(row, columns, rowIdx) {
   ));
 }
 
-function TableRowList(data, columns, detailsElement) {
+Table.propTypes = {
+  columns: PropTypes.arrayOf(
+    COLUMN_TYPE
+  ).isRequired,
+  row: DATA_TYPE.isRequired,
+  rowIdx: PropTypes.number.idRequired,
+};
+
+function TableRowList({ data, columns, detailsElement }) {
   const [openRows, setOpenRows] = useState([]);
 
   const handleClickRow = ({ target, currentTarget }) => {
     if (target.tagName !== 'A') {
       const clickedRowIndex = parseInt(currentTarget.dataset.key);
-      console.log(clickedRowIndex);
+
       if (openRows.includes(clickedRowIndex)) {
         setOpenRows(openRows.filter((i) => i !== clickedRowIndex));
       } else {
@@ -64,47 +82,43 @@ function TableRowList(data, columns, detailsElement) {
 
   return data.map((row, idx) => (
     <>
-      <tr
-        key={idx}
-        data-key={idx}
-        onClick={detailsElement && handleClickRow}
-        title='Click to show details'
-      >
-        {TableRow(row, columns, idx)}
+      <tr key={idx} data-key={idx} onClick={detailsElement && handleClickRow} title="Click to show details">
+        <TableRow row={row} columns={columns} idx={idx} />
       </tr>
-      <tr
-        key={idx + '-detail'}
-        className={'table__details ' + (!openRows.includes(idx) && 'collapsed')}
-      >
+      <tr key={idx + '-detail'} className={'table__details ' + (!openRows.includes(idx) && 'collapsed')}>
         <td colspan={1 + columns.length}>{callDetailsRenderer(row, idx)}</td>
       </tr>
     </>
   ));
 }
 
+TableRowList.propTypes = {
+  columns: PropTypes.arrayOf(
+    COLUMN_TYPE
+  ).isRequired,
+  data: PropTypes.arrayOf(DATA_TYPE).isRequired,
+  detailsElement: DETAILS_SECTION_TYPE,
+};
+
 export default function Table({ columns, data, detailsElement }) {
   return (
-    <table className='table' cellspacing='0' cellpadding='0'>
+    <table className="table" cellspacing="0" cellpadding="0">
       <thead>
-        <tr>{TableColumns(columns)}</tr>
+        <tr>
+          <TableColumns columns={columns} />
+        </tr>
       </thead>
-      <tbody>{TableRowList(data, columns, detailsElement)}</tbody>
+      <tbody>
+        <TableRowList data={data} columns={columns} detailsElement={detailsElement} />
+      </tbody>
     </table>
   );
 }
 
 Table.propTypes = {
-  columns: PropTypes.arrayOf(PropTypes.exact({
-    heading: PropTypes.string,
-    type: PropTypes.string,
-    col: PropTypes.string,
-    colLink: PropTypes.string,
-  })).isRequired,
-  data: PropTypes.arrayOf(PropTypes.object).isRequired,
-  detailsElement: PropTypes.oneOfType([
-    PropTypes.element,
-    PropTypes.string,
-    PropTypes.node,
-    PropTypes.func,
-  ]),
+  columns: PropTypes.arrayOf(
+    COLUMN_TYPE
+  ).isRequired,
+  data: PropTypes.arrayOf(DATA_TYPE).isRequired,
+  detailsElement: DETAILS_SECTION_TYPE,
 };
