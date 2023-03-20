3f0240f594abe21c786bd0c0009308d6f8ee8341
VanGamo <vangamo.beta@gmail.com>
2023-03-20 19:41:38 +0100

    Add plugins crud to backend.

--- server_api/src/app.py
+++ server_api/src/app.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource
 from flask_cors import CORS
 from src.db import *
 from src.services.sitesService import SitesService
+from src.services.wpPluginsService import WPPluginsService
 
 # PGSQL_WPCARE_HOST=localhost PGSQL_WPCARE_DB=wpcare PGSQL_WPCARE_USER= PGSQL_WPCARE_PASSWORD= bin/flask --app app run --debug
 
@@ -12,6 +13,7 @@ api = Api(app)
 CORS(app)
 
 sitesService = SitesService()
+pluginsService = WPPluginsService()
 
 @app.route('/')
 def get_root():
@@ -53,6 +55,36 @@ class Site(Resource):
     return response
 
 api.add_resource(Site, "/api/sites/", "/api/site/", "/api/site/<int:id>", "/api/site/<int:id>/")
+
+
+class WPPlugin(Resource):
+  def get(self, id:int=None):
+    
+    if id is None:
+      response = pluginsService.get_all()
+
+      return response
+    else:
+      response = pluginsService.get_one(id)
+
+      return response
+
+  def post(self):
+    response = pluginsService.insert(request.json)
+
+    return response
+
+  def put(self, id:int=0):
+    response = pluginsService.update(id, request.json)
+
+    return response
+
+  def delete(self, id:int=0):
+    response = pluginsService.delete(id)
+
+    return response
+
+api.add_resource(WPPlugin, "/api/plugins/", "/api/plugin/", "/api/plugin/<int:id>", "/api/plugin/<int:id>/")
 
 # if __name__ == '__main__':
 #   app.run(debug=True)
