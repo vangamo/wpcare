1dcb3504e2436cb6300e2cb1be0ef0c106313cc5
VanGamo <vangamo.beta@gmail.com>
2023-03-29 10:34:35 +0200

    Highlight the menu option that corresponds to the current section.

--- frontend_admin/src/components/layout/menu.jsx
+++ frontend_admin/src/components/layout/menu.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import FeatherIcon from 'feather-icons-react/build/FeatherIcon';
 import PropTypes from 'prop-types';
 
@@ -7,22 +7,22 @@ export default function Menu({ showMenu }) {
     <section className={'sidenav shadow-right ' + (!showMenu && 'hidden')}>
       <nav className="sidenav_menu">
         <div className="sidenav__menuheading">Overview</div>
-        <Link className="sidenav__menulink active" to="/" aria-current="page">
+        <NavLink className="sidenav__menulink" to="/" aria-current="page">
           <span className="sidenav__menulink--icon">
             <FeatherIcon icon="activity" size="24" />
           </span>
           Dashboard
-        </Link>
+        </NavLink>
         {/*
-        <Link className="sidenav__menulink">
+        <NavLink className="sidenav__menulink">
           <span className="sidenav__menulink--icon">
             <FeatherIcon icon="clock" size="24" />
           </span>
           Load times
-        </Link>
+        </NavLink>
         */}
         <div className="sidenav__menuheading">Webs</div>
-        <Link className="sidenav__menulink" to="/sites">
+        <NavLink className="sidenav__menulink" to="/sites">
           <span className="sidenav__menulink--icon">
             <FeatherIcon icon="folder" size="24" />
           </span>
@@ -30,9 +30,9 @@ export default function Menu({ showMenu }) {
           <span className="sidenav__menulink--arrow">
             <FeatherIcon icon="chevron-right" size="24" />
           </span>
-        </Link>
+        </NavLink>
         {/*
-        <Link className="sidenav__menulink">
+        <NavLink className="sidenav__menulink">
           <span className="sidenav__menulink--icon">
             <FeatherIcon icon="layout" size="24" />
           </span>
@@ -40,8 +40,8 @@ export default function Menu({ showMenu }) {
           <span className="sidenav__menulink--arrow">
             <FeatherIcon icon="chevron-right" size="24" />
           </span>
-        </Link>
-        <Link className="sidenav__menulink" to="/clients/">
+        </NavLink>
+        <NavLink className="sidenav__menulink" to="/clients/">
           <span className="sidenav__menulink--icon">
             <FeatherIcon icon="briefcase" size="24" />
           </span>
@@ -49,10 +49,10 @@ export default function Menu({ showMenu }) {
           <span className="sidenav__menulink--arrow">
             <FeatherIcon icon="chevron-right" size="24" />
           </span>
-        </Link>
+        </NavLink>
         */}
         <div className="sidenav__menuheading">WP</div>
-        <Link className="sidenav__menulink" to="/plugins/">
+        <NavLink className="sidenav__menulink" to="/plugins/">
           <span className="sidenav__menulink--icon">
             <FeatherIcon icon="slider" size="24" />
           </span>
@@ -60,15 +60,15 @@ export default function Menu({ showMenu }) {
           <span className="sidenav__menulink--arrow">
             <FeatherIcon icon="chevron-right" size="24" />
           </span>
-        </Link>
+        </NavLink>
         <div className="sidenav__menuheading">Tests</div>
         <div className="sidenav__menuheading">Tools</div>
-        <Link className="sidenav__menulink" to="/backup">
+        <NavLink className="sidenav__menulink" to="/backup">
           <span className="sidenav__menulink--icon">
             <FeatherIcon icon="package" size="24" />
           </span>
           Import/Export
-        </Link>
+        </NavLink>
       </nav>
       <dl className="sidenav_footer">
         <dt className="sidenav_footer--subtitle">Logged in as:</dt>
